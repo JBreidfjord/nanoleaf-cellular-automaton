5f8a48d8c3d7014fde2c93e4e205f2ad381b4f50
Evan Mann <ejmann900@gmail.com>
2022-09-23 20:54:17 -0600

    merged new functions

--- nanoleaf_hackathon/evanWork.py
+++ nanoleaf_hackathon/evanWork.py
@@ -1,17 +1,4 @@
-import random
 
 
-def create_rand_board(gameboard):
-    cells_to_turn = random.randrange(100, 131)
-    for i in range(0, cells_to_turn):
-        good_cell = False
-        while good_cell == False:
-            turning_cell_row = random.randrange(0, 12)
-            turning_cell_col = random.randrange(0, 23)
-            if gameboard[turning_cell_row][turning_cell_col] == "x" or gameboard[turning_cell_row][turning_cell_col] == "alive":
-                good_cell = False
-            else:
-                good_cell = True
-        update_game_panel(turning_cell_row, turning_cell_col, "alive", gameboard)
 
 
--- nanoleaf_hackathon/gamecode.py
+++ nanoleaf_hackathon/gamecode.py
@@ -1,4 +1,4 @@
-
+import random
 dead_gameboard=[ #x means the tile does not exit, dead means the cell is dead and alive means it's fucking alive dibshit
     ['x','x','x','x','x','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','x','x','x','x','x'],
     ['x','x','x','x','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','dead','x','x','x','x'],
@@ -42,3 +42,34 @@ def update_game_12(gameboard):
                 if num_alive >= 6:
                     gameboard[row_num][col_num == 'dead']
     return gameboard
+
+
+def create_rand_board(gameboard):
+    cells_to_turn = random.randrange(100, 131)
+    for i in range(0, cells_to_turn):
+        good_cell = False
+        while good_cell == False:
+            turning_cell_row = random.randrange(0, 12)
+            turning_cell_col = random.randrange(0, 23)
+            if gameboard[turning_cell_row][turning_cell_col] == "x" or gameboard[turning_cell_row][turning_cell_col] == "alive":
+                good_cell = False
+            else:
+                good_cell = True
+        update_game_panel(turning_cell_row, turning_cell_col, "alive", gameboard)
+
+
+def test_board(current_game):
+    x=0 #initialise the counter
+    while x < 100 and is_dead == False:
+        current_game = update_game_12(current_game)
+        for row_num in range(len(current_game)):
+            for col_num in range(len(current_game[row_num])):
+                if current_game[row_num][col_num] == 'alive':
+                    is_dead = False
+                    break
+            if is_dead == False:
+                break
+    if is_dead:
+        return is_dead
+    else:
+        return True
